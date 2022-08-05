38b5fb74f119496483fff0fea927f539bd68e601
grmt <garmt.noname@gmail.com>
2022-08-05 16:53:31 +0200

    sync with latest supabase origin develop, add work_mem variable default 4MB -> 32MB to speed up events function, bump postgres to 14.4

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,3 @@
-docker build . --build-arg PLATFORM=linux/amd64 --build-arg VERSION=14.2 -t key2asset/postgres
+docker build . --build-arg PLATFORM=linux/amd64 --build-arg VERSION=14 -t key2asset/postgres
 docker tag key2asset/postgres key2asset/postgres:develop-latest
 docker push key2asset/postgres:develop-latest
