d8ad2928bf5e938d6477705a3b1e5b5aa9bf6041
grmt <garmt.noname@gmail.com>
2022-11-15 13:01:17 +0100

    fix docker tag

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,5 @@
-docker build . --build-arg PLATFORM=linux/amd64 --build-arg VERSION=14.5 -t key2asset/postgres 2>&1 | tee build_docker.log || (echo  "build failed" && exit 1)
-docker tag key2asset/postgres key2asset/postgres:develop-latest
-docker push key2asset/postgres:develop-latest
-# docker run -e POSTGRES_PASSWORD=postgres key2asset/postgres
+TAG=$(git log -1 --pretty=format:%h)
+docker build . --build-arg PLATFORM=linux/amd64 --build-arg VERSION=14.5 -t key2asset/postgres:${TAG} 2>&1 | tee build_docker.log || (echo  "build failed" && exit 1)
+docker tag key2asset/postgres:{TAG} key2asset/postgres:develop-latest
+docker push key2asset/postgres:{TAG}
+# docker run -e POSTGRES_PASSWORD=postgres key2asset/postgres:develop-latest
