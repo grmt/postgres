98097140112487dca4bae076ea79973cceeaceff
grmt <garmt.noname@gmail.com>
2022-11-24 21:55:40 +0100

    improve build_docker.sh script, remove plv8

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,7 @@
+set -o pipefail
 TAG=$(git log -1 --pretty=format:%h)
-docker build . --build-arg PLATFORM=linux/amd64 --build-arg VERSION=14.5 -t key2asset/postgres:${TAG} 2>&1 | tee build_docker.log || (echo  "build failed" && exit 1)
-docker tag key2asset/postgres:{TAG} key2asset/postgres:develop-latest
-docker push key2asset/postgres:{TAG}
+(docker build . --build-arg PLATFORM=linux/amd64 --build-arg VERSION=14.6 -t key2asset/postgres:${TAG} 2>&1 | tee build_docker.log) || (echo  "build failed" && exit 1)
+docker tag key2asset/postgres:${TAG} key2asset/postgres:develop-latest
+docker push key2asset/postgres:${TAG}
 docker push key2asset/postgres:develop-latest
 # docker run -e POSTGRES_PASSWORD=postgres key2asset/postgres:develop-latest
